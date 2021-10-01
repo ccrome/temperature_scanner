0969c337ec1b2c967da58505764a51cea9260960
Caleb Crome <caleb@crome.org>
2021-10-01 08:07:52 -0700

    Added temperature sensor

--- scanner/scan.py
+++ scanner/scan.py
@@ -7,6 +7,7 @@ import numpy as np
 import re
 import threading
 import logging
+import time
 
 
 def close_enough(a, b, tolerance=0.01):
@@ -40,6 +41,41 @@ STATE_DISCONNECTED          = 1
 STATE_WAITING_FOR_STARTUP   = 2
 STATE_FIRST_MESSAGE_ARRIVED = 3
 STATE_IS_HOMED              = 5
+
+class TempScanner(LineReader):
+    def connection_made(self, transport):
+        super(TempScanner, self).connection_made(transport)
+        self._temperature = 0
+        self._temp_count = 0
+
+    def handle_line(self, data):
+        try:
+            data = int(data[2:], 16)
+            t = self.get_temp(data)
+            if t is not None:
+                self._temperature = t
+                self._temp_count += 1
+        except:
+            pass
+
+    def temperature(self, timeout=2):
+        start = self._temp_count
+        starttime = time.time()
+        while self._temp_count == start:
+            time.sleep(0.01)
+            if time.time() > starttime+timeout:
+                raise Exception("Timed out waiting for temperature update!")
+        return self._temperature
+    
+    def connection_lost(self, exc):
+        fprint(f"Temp scanner conneciton lost {exc}")
+
+    def get_temp(self, x):
+        if (x >> 32) & 0xFF == 0x4c:
+            return (x>> 16) & 0xFFFF
+        else:
+            return None
+        
 
 class GcodePrinter(LineReader):
     def connection_made(self, transport):
@@ -137,7 +173,7 @@ class GcodePrinter(LineReader):
 
 
 class Scanner:
-    def __init__(self, printer_port, printer_baud, safe_z, scan_z):
+    def __init__(self, printer_port, printer_baud, safe_z, scan_z, temp_port, temp_baud):
         self.ser = serial.Serial(printer_port, printer_baud, timeout=0.5)
         self.reader_thread = ReaderThread(self.ser, GcodePrinter)
         self.reader_thread.start()
@@ -146,7 +182,14 @@ class Scanner:
         self.scan_z = scan_z
         self.present_x = 0
         self.present_y = 280
+
+        self.temp_ser = serial.Serial(temp_port, temp_baud, timeout=5)
+        self.temp_reader_thread = ReaderThread(self.temp_ser, TempScanner)
+        self.temp_reader_thread.start()
+        self.temp_protocol = self.temp_reader_thread.protocol
+
         self.protocol.home()
+        # self.protocol.write_line(f"M140 S60")
 
     def scan(self, start_x, start_y, delta_x, delta_y, stepsize):
         fprint("Scanning")
@@ -158,9 +201,9 @@ class Scanner:
         while x <= start_x + delta_x:
             y = start_y
             while y <= start_y + delta_y:
-                fprint(f"({x:4},{y:4})")
                 p.goto(x=x, y=y)
-                time.sleep(0.2)
+                temperature = self.temp_protocol.temperature()
+                fprint(f"{x:4},{y:4},{temperature}")
                 y += stepsize
             x += stepsize
         p.goto(z=self.safe_z)
@@ -180,6 +223,8 @@ if __name__ == "__main__":
         p.add_argument("-g",  "--gridsize", help="Scanning grid step", type=int, required=True)
         p.add_argument("-sz", "--safe-z", help="Safe Z for all moves. Default=45", type=int, default=45)
         p.add_argument("-sn", "--scan-z", help="Scan Z during actual scanning. Default=10", type=int, default=10)
+        p.add_argument("-tp", "--temp-port", help="Temperature scanner port", required=True)
+        p.add_argument("-tb", "--temp-baud", help="Temperature baud", type=int, default=115200)
         p.add_argument("--logfile", help="name of logfile to dump to.  Default is scanner.log", default="scanner.log")
         args = p.parse_args()
         return args
@@ -187,6 +232,7 @@ if __name__ == "__main__":
     args = get_args()
     fprint("Starting Scan. Homing")
     logging.basicConfig(filename=args.logfile, encoding='utf-8', level=logging.DEBUG)
-    s = Scanner(args.printer_port, args.printer_baud, args.safe_z, args.scan_z)
+    s = Scanner(args.printer_port, args.printer_baud, args.safe_z, args.scan_z,
+                args.temp_port, args.temp_baud)
     fprint("Homing complete")
     s.scan(args.start_x, args.start_y, args.delta_x, args.delta_y, args.gridsize)
